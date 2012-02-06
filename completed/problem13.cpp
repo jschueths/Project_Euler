5ec1228282546d24d3f09154355ef6c6b3b9e908
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-02-06 12:02:20 -0600

    solved problem13, started a NN_DIGIT class(works for addition)

--- completed/problem13.cpp
+++ completed/problem13.cpp
@@ -2,18 +2,33 @@
 // Description: Problem 13 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include <ifstream>
+#include <fstream>
+
+#include "nn_digit.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+  NN_DIGIT nums[100];
+  
   if(argc != 2)
   {
     std::cerr << "Incorrect Usage" << std::endl;
     return 1;
   }
-  
   ifstream in;
   in.open(argv[1]);
+  
+  for(int i = 0; i < 100; i++)
+  {
+    in >> nums[i];
+  }
+  for(int i = 1; i < 100; i++)
+  {
+    nums[0] += nums[i];
+  }
+  cout << nums[0] << endl;
   
   return 0;
 }
