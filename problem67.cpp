c6bf5822ec403ad50ed6a00bae9dac58dbd1e775
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-02-10 11:15:50 -0600

    Worked on problem 67

--- problem67.cpp
+++ problem67.cpp
@@ -4,18 +4,59 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+void max_path(unsigned int i, unsigned int j, vector<vector<int> > &triangle, unsigned int &sum);
 
 int main(int argc, char *argv[])
 {
   ifstream in;
-  unsigned int product;
-  unsigned int max_path;
-  string input, temp;
+  unsigned int sum = 0;
+  vector<vector<int> > my_triangle;
+  vector<int> temp_v;
+  unsigned int index_i = 0;
+  unsigned int index_j = 0;
+  int temp;
   if(argc != 2)
   {
     std::cerr << "Incorrect Usage" << std::endl;
     return 1;
   }
+  in.open(argv[1]);
+  int level = 1;
+  while(!in.eof())
+  {
+    for(int i = 0; i < level; i++)
+    {
+      in >> temp;
+      temp_v.push_back(temp);
+    }
+    my_triangle.push_back(temp_v);
+    temp_v.clear();
+    level++;
+  }
+  in.close();
   
+  max_path(index_i, index_j, my_triangle, sum);
+  cout << "Max Sum: " << sum << endl;
+  
+  return 0;
+}
+
+void max_path(unsigned int i, unsigned int j, vector<vector<int> > &triangle, unsigned int &sum)
+{
+  if(i == triangle.size() - 1)
+  {
+    sum += triangle[i][j];
+    return;
+  }
+  cout << i << "\t" << j << endl;
+  sum += triangle[i][j];  
+  if(triangle[i+1][j] > triangle[i+1][j+1])
+    max_path(++i, j, triangle, sum);
+  else
+    max_path(++i, ++j, triangle, sum);
+  return;
+}
---
+++ problem75.cpp
@@ -0,0 +1,3 @@
+//      Author: jschueths
+// Description: Problem 8 of the Euler Project (projecteuler.net/problems)
+
