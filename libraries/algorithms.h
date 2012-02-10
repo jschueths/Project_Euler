a9be22c368dd8e9723e650c2145b7565740e643c
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-02-10 09:49:58 -0600

    Completed problem 10

---
+++ libraries/algorithms.h
@@ -0,0 +1,15 @@
+//      Author: jschueths
+// Description: A function library for mathematical algorithms.
+
+#ifndef __ALGORITHM_H__
+#define __ALGORITHM_H__
+
+#include <cmath>
+
+bool is_prime(unsigned long long int x);
+
+
+
+#include "algorithms.hpp"
+
+#endif
