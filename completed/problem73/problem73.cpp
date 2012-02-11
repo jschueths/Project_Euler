a8896d97aafe889542eb98cd25e2d884a8885722
Joshua Schueths <josh.schueths@gmail.com>
2012-02-11 17:46:02 -0600

    Completed problem 73

--- completed/problem73/problem73.cpp
+++ completed/problem73/problem73.cpp
@@ -12,19 +12,17 @@ int main()
   int comp1, comp2;
   unsigned int count = 0;
   double temp;
-  
-  cout << HCF(3, 4) << endl;
-  cout << HCF(2, 6) << endl;
+
 
   for(unsigned int i = 4; i <= 12000; i++)
   {
-    for(unsigned int j = 1 / 3; j < (i/2); j++)
+    for(unsigned int j = (1 / 3) ; j < ((i/2) + 1); j++)
     {
       temp = (double)j / i;
       if((temp < 0.5 && temp > (1/3.0)) && HCF(j, i) == 1)
       {
         count++;
-        cout << "Count: " << count << "\t" << j << "\t" << i << endl;
+        //cout << "Count: " << count << "\t" << j << "\t" << i << endl;
       }
     }
   }
