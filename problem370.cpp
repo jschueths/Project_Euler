3fc33bb18be3c62bcc0f5f8954373360119f9169
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-02-13 09:36:55 -0600

    Completed problem 24

--- problem370.cpp
+++ problem370.cpp
@@ -9,12 +9,11 @@ using namespace std;
 int main()
 {
   unsigned long int count = 0;
-  long double perimeter = 2.5 * pow(10, 13);
+  long double perimeter = pow(10, 6);
   for(unsigned long long int c = (perimeter / 3); c < (perimeter - 3); c++)
   {
     for(unsigned long long int b = c; b > 1; b--)
     {
-      cout << b << endl;
       for(unsigned long long int a = b; a > 0; a--)
       {
         if(a + b + c == perimeter)
