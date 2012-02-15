72ee8dacf1d9326ea45ad4983240ba2df9c8af30
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-02-15 15:27:14 -0600

    Completed problem 70

---
+++ completed/problem70/problem70.cpp
@@ -0,0 +1,40 @@
+//      Author: jschueths
+// Description: Problem 72 of the Euler Project (eulerproject.net/problems)
+
+#include "./libraries/algorithms.h"
+#include "./libraries/timer.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  Timer my_time;
+  unsigned long long int count = 0;
+  unsigned int limit = 10000000;
+  vector<unsigned long int> totient;
+  long double ratio = 87109. / 79810;
+  long double temp;
+  unsigned int n;
+  for(unsigned int i = 0; i < limit; i++)
+    totient.push_back(i);
+  phi(totient);
+  my_time.start();
+  for(unsigned int d = 2; d < limit; d++)
+  {
+    temp = (double) d / totient[d];
+    if(is_permutation(d, totient[d]))
+    {
+      if(temp < ratio)
+      {
+        ratio = temp;
+        n = d;
+      }
+    }
+  }
+  my_time.end();
+  cout << "Solution: " << n << endl;
+  cout << "Elapsed Time: " << my_time << " seconds" << endl;
+  return 0;
+}
