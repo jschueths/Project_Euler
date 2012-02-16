43b4268f4b8c31f3e39de01de35a7763e0ce09f5
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-02-16 14:00:37 -0600

    Added commentation and timer to problem 3

--- completed/problem3/problem3.cpp
+++ completed/problem3/problem3.cpp
@@ -2,44 +2,31 @@
 // Description: Problem 3 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include <cmath>
+#include "./libraries/timer.h"
+#include "./libraries/algorithms.h"
 
 using std::cout;
 using std::endl;
 
-bool is_prime(unsigned long long int x);
-
 int main()
 {
-  unsigned long long int num = 600851475143;
+  unsigned long long int num = 600851475143; // Num provided by the problem.
   unsigned long long int largest_prime;
-  unsigned long long int limit = (num / 2) + 1;
+  unsigned long long int limit = (num / 2) + 1; // To find a factor, we only need to go to half of the number;
   unsigned long long int temp = 6;
   unsigned long long int i = 3;
+  Timer my_timer;
+  my_timer.start();
   while(temp < limit)
   {
+    // If the current value is prime and a factor, save it as the
+    // largest prime found so far.
     if(is_prime(temp) && !(num % temp))
       largest_prime = temp;
     temp++;
   }
+  my_timer.end();
   cout << "Largest Prime Factor: " << largest_prime << endl;
+  cout << "Time Elapsed: " << my_timer << " seconds" << endl;
   return 0;
-}
-
-bool is_prime(unsigned long long int x)
-{
-  unsigned long long int i = 1;
-  if(!(x % 2))
-    return false;
-  if(!(x % 3))
-    return false;
-  while((6 * i) - 1 < sqrt(x) + 1)
-  {
-    if(!(x % ((6*i) + 1)))
-      return false;
-    if(!(x % ((6*i) - 1)))
-      return false;
-    i++;
-  }
-  return true;
 }
