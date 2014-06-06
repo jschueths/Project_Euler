09ff6db0c7aa9865e03d024884383fe03df4348d
Josh Schueths <josh.schueths@gmail.com>
2014-06-06 14:17:24 -0500

    Convert problem8 to cmake; optimize problem8

--- src/problem8/problem8.cpp
+++ src/problem8/problem8.cpp
@@ -2,41 +2,45 @@
 // Description: Problem 8 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include <fstream>
 #include <string>
-#include "./libraries/nn_digit.h"
+#include <boost/lexical_cast.hpp>
 
-using namespace std;
+const std::string LARGE_NUMBER = "73167176531330624919225119674426574742355349194934"
+				 "96983520312774506326239578318016984801869478851843"
+				 "85861560789112949495459501737958331952853208805511"
+				 "12540698747158523863050715693290963295227443043557"
+				 "66896648950445244523161731856403098711121722383113"
+				 "62229893423380308135336276614282806444486645238749"
+				 "30358907296290491560440772390713810515859307960866"
+				 "70172427121883998797908792274921901699720888093776"
+				 "65727333001053367881220235421809751254540594752243"
+				 "52584907711670556013604839586446706324415722155397"
+				 "53697817977846174064955149290862569321978468622482"
+				 "83972241375657056057490261407972968652414535100474"
+				 "82166370484403199890008895243450658541227588666881"
+				 "16427171479924442928230863465674813919123162824586"
+				 "17866458359124566529476545682848912883142607690042"
+				 "24219022671055626321111109370544217506941658960408"
+				 "07198403850962455444362981230987879927244284909188"
+				 "84580156166097919133875499200524063689912560717606"
+				 "05886116467109405077541002256983155200055935729725"
+				 "71636269561882670428252483600823257530420752963450";
 
-int main(int argc, char *argv[])
-{
-  ifstream in;
-  unsigned int product;
-  unsigned int max_product = 0;
-  string input, temp;
-  if(argc != 2)
-  {
-    std::cerr << "Incorrect Usage" << std::endl;
-    return 1;
-  }
-  
-  in.open(argv[1]);
-  in >> input;
-  while(!in.eof())
-  {
-    in >> temp;
-    input += temp;
-  }
-  in.close();
-  NN_DIGIT data(input);
-  
-  for(int i = 0; i < data.size() - 5; i++)
-  {
-    product = data[i] * data[i + 1] * data[i + 2] * data[i + 3] * data[i +4];
-    if(product > max_product)
-      max_product = product;
-  }
-  
-  std::cout << "Max product: " << max_product << std::endl;
-  return 0;
+const size_t SUBSTR_LEN = 13;
+
+int main() {
+	size_t maxProduct = 0;
+	for(size_t i = 0; i < LARGE_NUMBER.size() - SUBSTR_LEN; ++i) {
+		std::string sub(LARGE_NUMBER.substr(i, SUBSTR_LEN));
+		size_t product = 1;
+		for(auto it = sub.begin(); it < sub.end(); it++) {
+			product *= boost::lexical_cast<size_t>(*it);
+		}
+		if(product > maxProduct) {
+			maxProduct = product;
+		}
+	}
+	std::cout << "Max product: " << maxProduct << std::endl;
+	return 0;
 }
+
