b7b1c84f73531070755f5e7b4d69e81cfbf29ce3
Josh Schueths <josh.schueths@gmail.com>
2014-06-06 10:31:38 -0500

    Convert problem2 to cmake

--- src/problem2/problem2.cpp
+++ src/problem2/problem2.cpp
@@ -2,32 +2,28 @@
 // Description: Problem 2 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include "./libraries/timer.h"
-int main()
-{
-  // Init the variables
-  long int sum;
-  long int last_val;
-  long int cur_val = 1;
-  long int next_val = 2;
-  Timer my_timer;
-  my_timer.start();
-  sum = next_val;
+
+int main(){
+	// Init the variables
+	long int sum;
+	long int last_val;
+	long int cur_val = 1;
+	long int next_val = 2;
+	sum = next_val;
   
-  while(next_val < 4000000)
-  {
-    last_val = cur_val;
-    
-    // Generate the next term in the Fibonacci sequence
-    cur_val = next_val;
-    next_val = cur_val + last_val;
-    
-    // If the term is even, add it to the sum;
-    if(!(next_val % 2))
-      sum += next_val;
-  }
-  my_timer.end();
-  std::cout << "Sum: " << sum << std::endl;
-  std::cout << "Time Elapsed: " << my_timer << " seconds" << std::endl;
-  return 0;
+	while(next_val < 4000000){
+		last_val = cur_val;
+
+		// Generate the next term in the Fibonacci sequence
+		cur_val = next_val;
+		next_val = cur_val + last_val;
+
+		// If the term is even, add it to the sum;
+		if(!(next_val % 2)){
+			sum += next_val;
+		}
+	}
+	std::cout << "Sum: " << sum << std::endl;
+	return 0;
 }
+
