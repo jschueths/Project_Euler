090d419b0f07d4fe3353e99b641def97a52b3f53
Josh Schueths <josh.schueths@gmail.com>
2014-06-06 18:25:48 -0500

    Convert problem16 to cmake

--- src/problem16/problem16.cpp
+++ src/problem16/problem16.cpp
@@ -2,22 +2,21 @@
 // Description: Problem 16 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include "./libraries/nn_digit.h"
+#include <string>
+#include <boost/multiprecision/cpp_int.hpp>
 
-int main()
-{
-  int coeff = 2;
-  NN_DIGIT my_pw_2("2");
-  unsigned int sum = 0;
-  for(int i = 1; i < 1000; i++)
-  {
-    my_pw_2 *= coeff;
-  }
-  for(unsigned int i = 0; i < my_pw_2.size(); i++)
-  {
-    sum += my_pw_2[i];
-  }
-  
-  std::cout << "Sum: " << sum << std::endl;
-  return 0;
+int main() {
+	boost::multiprecision::cpp_int result = 2;
+
+	for(size_t i = 1; i < 1000; ++i) {
+		result *= 2;
+	}
+	size_t sum = 0;
+	std::string result_str = result.str();
+	for(auto it = result_str.begin(); it != result_str.end(); it++) {
+		sum += boost::lexical_cast<size_t>(*it);
+	}
+	std::cout << "Sum: " << sum << std::endl;
+	return 0;
 }
+
