5909a3ecb57c432c0bbe0003ca3b22d1c1518e85
Josh Schueths <josh.schueths@gmail.com>
2014-06-06 10:07:24 -0500

    Move all completed; remove duplicate library copies

--- src/problem138/problem138.cpp
+++ src/problem138/problem138.cpp
@@ -2,42 +2,62 @@
 // Description: Problem 138 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include <cmath>
+#include <vector>
+#include "./libraries/timer.h"
 #include "./libraries/algorithms.h"
 
 using namespace std;
 
 int main()
 {
-  unsigned int b = 2;
-  unsigned int sum = 0;
-  unsigned int h;
-  long double L;
-  int count = 0;
-  cout << "b" << "\t" << "h" << "\t" << "L" << "\t" << "Count" << "\t" << "Sum" << endl;
-  while(count < 30)
+  Timer my_time;
+  
+  long long int sum = 0;
+  long long int base, height;
+  long long length;
+  long long int mult = 1;
+  
+  vector<long int> fib_num;
+  fib_num.push_back(1);
+  fib_num.push_back(1);
+  for(long int i = 1; i < 37; i++)
+    fib_num.push_back(fib_num[i - 1] + fib_num[i]);
+  vector<long long int> n, m;
+  vector<long long int> sum2;
+
+  for(unsigned int i = 3; i < fib_num.size(); i++)
   {
-    h = b + 1;
-    L = sqrt(((b * b)/4.0) + (h * h));
-    if(floor(L + 0.5) == L)
+    switch((i-3) % 3)
     {
-      sum += L;
-      count++;
-      cout << b << "\t" << h << "\t" << L << "\t" << count << "\t" << sum << endl;
+      case 0:
+        n.push_back(fib_num[i]);
+        break;
+      case 1:
+        m.push_back(fib_num[i]);
+        break;
     }
-    
-    h = b - 1;
-    L = sqrt(((b * b)/4.0) + (h * h));
-    if(floor(L + 0.5) == L)
-    {
-      sum += L;
-      count++;
-      cout << b << "\t" << h << "\t" << L << "\t" << count << "\t" << sum << endl;
-    }
-    b++;
   }
-  if(count == 13)
-    sum -= L;
+  
+  my_time.start();
+  for(int i = 0; i < 12; i++)
+  {
+    mult = 1;
+    height = m[i] * n[i];
+    base = ((m[i] * m[i]) - (n[i] * n[i])) / 2;
+    length = ((m[i] * m[i]) + (n[i] * n[i])) / 2;
+    int result;
+    result = (base * 2) - height;
+    while(result != 1 && result != -1)
+    {
+      mult++;
+      result = ((base * 2)) * mult - (height * mult);
+    }
+    sum = sum + (length * mult);
+    sum2.push_back(length * mult);
+  }
+  my_time.end();
+  
   cout << "Sum: " << sum << endl;
+  cout << "Time Elapsed: " << my_time << " seconds" << endl;
   return 0;
 }
