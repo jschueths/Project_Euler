fabe944b49064af27703f2b86aee3aa837ab7a4f
Josh Schueths <josh.schueths@gmail.com>
2014-06-06 13:58:35 -0500

    Convert problem7 to cmake

--- src/problem7/problem7.cpp
+++ src/problem7/problem7.cpp
@@ -3,49 +3,21 @@
 
 #include <iostream>
 #include <cmath>
-#include <ctime>
+#include <prime.h>
 
-using namespace std;
+int main(){
+	size_t num = 6;
+	size_t prime;
+	size_t count = 3;
 
-bool is_prime(unsigned long long int &x);
-
-int main()
-{
-  clock_t start, end;
-  unsigned long long int num = 6;
-  unsigned long long int prime;
-  unsigned int count = 3;
-  start = clock();
-  while(count < 10001)
-  {
-    if(is_prime(num))
-    {
-      prime = num;
-      count++;
-    }
-    num++;
-  }
-  end = clock();
-  cout << "10001st Prime: " << prime << endl;
-  cout << "Elapsed Time: " << ((double) (end - start) / CLOCKS_PER_SEC) << " sec" << endl;
-  return 0;
+	while(count < 10001) {
+		if(isPrime(num)) {
+			prime = num;
+			count++;
+		}
+		num++;
+	}
+	std::cout << "10001st Prime: " << prime << std::endl;
+	return 0;
 }
 
-
-bool is_prime(unsigned long long int &x)
-{
-  unsigned long long int i = 1;
-  if(!(x % 2))
-    return false;
-  if(!(x % 3))
-    return false;
-  while((6 * i - 1) < (sqrt(x) + 1))
-  {
-    if(!(x % ((6 * i) - 1)))
-      return false;
-    if(!(x % ((6 * i) + 1)))
-      return false;
-    i++;
-  }
-  return true;
-}
