9f058373b1f647a4a9f6dc7f68a8f6c1bb5e9670
Josh Schueths <josh.schueths@gmail.com>
2014-06-06 23:53:27 -0500

    Convert problem29 to cmake

--- src/problem29/problem29.cpp
+++ src/problem29/problem29.cpp
@@ -1,45 +1,21 @@
 //      Author: jschueths
-// Description: Problem 3 of the Euler Project (projecteuler.net/problems)
+// Description: Problem 29 of the Euler Project (projecteuler.net/problems)
 
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <set>
+#include <boost/multiprecision/cpp_int.hpp>
 
-using std::vector;
+int main() {
+	std::set<boost::multiprecision::cpp_int> result;
+	for(size_t i = 2; i <= 100; i++) {
+		for(size_t j = 2; j <= 100; j++) {
+			result.insert(
+				boost::multiprecision::pow(
+					boost::multiprecision::cpp_int(i), j));
+		}
+	}
 
-void save_if_new(long double x, vector<long double> &l);
-
-int main()
-{
-  vector<long double> list;
-  long double temp;
-  for(long double i = 2; i <= 100; i++)
-  {
-    for(long double j = 2; j <=100; j++)
-    {
-      temp = pow(i, j);
-      save_if_new(temp, list);
-    }
-  }
-  
-  std::cout << "Total Distint Values: " << list.size() << std::endl;
-  return 0;
+	std::cout << "Total Distint Values: " << result.size() << std::endl;
+	return 0;
 }
 
-void save_if_new(long double x, vector<long double> &l)
-{
-  bool found = false;
-  long double size = l.size();
-  for(long double i = 0; i < size; i++)
-  {
-    if(x == l[i])
-    {
-      found = true;
-      break;
-    }
-  }
-  
-  if(!found)
-    l.push_back(x);
-}
-  
